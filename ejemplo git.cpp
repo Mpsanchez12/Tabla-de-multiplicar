c9ea2d499f66f88a00cc82c9d050992b4be9a44c
Mpsanchez12 <165180096+Mpsanchez12@users.noreply.github.com>
2024-09-12 19:46:02 -0300

    Update ejemplo git.cpp

--- ejemplo git.cpp
+++ ejemplo git.cpp
@@ -1,19 +1,26 @@
-#include <iostream>
-using namespace std;
+void tablademultiplicacion() {
+    int limite = 0, numeroentero = 0;
 
-int main (){
-int numero;
-cout<<"Ingresar numero";
-cin>>numero;
-for(int i=1;i<=10;i++){
- cout<<numero<<"x"<<i<<"="<<numero*i<<endl;
-}
+  
+    do {
+        cout << "Ingrese numero entero positivo: ";
+        cin >> numeroentero;
+        if (numeroentero <= 0) {
+            cout << "El numero debe ser positivo." << endl;
+        }
+    } while (numeroentero <= 0);
 
+    do {
+        cout << "Ingrese el limite positivo: ";
+        cin >> limite;
+        if (limite <= 0) {
+            cout << "El limite debe ser positivo." << endl;
+        }
+    } while (limite <= 0);
 
-
-
-
-
-return 0;
-
+   
+    cout << "Tabla de multiplicacion del " << numeroentero << ":" << endl;
+    for (int i = 1; i <= limite; i++) {
+        cout << numeroentero << " x " << i << " = " << numeroentero * i << endl;
+    }
 }
